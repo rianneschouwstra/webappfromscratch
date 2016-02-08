66141cbee5a4c2160426684a1816c8540cc8a6ff
Rianne Schouwstra <rianne.schouwstra@hva.nl>
2016-02-08 19:49:32 +0100

    app only for utrecht

---
+++ opdracht5week2/static/app.utrecht.js
@@ -0,0 +1,76 @@
+// Source: Rover van Nispen: https://github.com/rovervannispen/Everything-Web-Minor/blob/gh-pages/web-app-from-scratch/assignments/opdracht%205/static/js/app.js
+// Source: Sem Bakkum (sections.toggle - for)
+
+(function(){
+	'use strict'
+
+	var startDisplay = document.getElementById('start');
+	var utrechtDisplay = document.getElementById('weatherUtrecht');
+
+	var app = {
+		init: function(){
+
+			routie({
+
+				'start': function(){
+					console.log('test');
+					sections.toggle('start');
+				},
+				'weatherUtrecht': function(){
+					sections.toggle('weatherUtrecht');
+					weather.init();
+				},
+				'*': function(){
+					startDisplay.classList.add('active');	
+				}
+			});
+
+		}
+	};
+
+	var weather = {
+		init: function(){
+
+			var template = document.getElementById('weatherUtrecht');
+
+			microAjax('http://api.openweathermap.org/data/2.5/weather?id=2745912&units=metric&appid=44db6a862fba0b067b1930da0d769e98', function(data){
+				data = JSON.parse(data);
+
+				console.log(data);
+
+				var templateData = {
+					city: data.name,
+					temp: data.main.temp,
+					windSpeed: data.wind.speed,
+					weatherType: data.weather[0].description
+				}
+
+				console.log(templateData);
+
+				Transparency.render(template, templateData);
+			});
+
+		}
+	};
+
+	var sections = {
+		toggle: function(route){
+
+			var allSections = document.querySelectorAll('section');
+			var toggleSection = document.getElementById(route);
+
+			console.log(allSections);
+			// Source For Loop Sem Bakkum: https://github.com/SemBakkum/SemBakkum.github.io/tree/master/WAFS/Week%201/Exercise%205
+			for (var c = 0; c < allSections.length; c++) {
+				allSections[c].classList.remove('active');
+			}
+
+			toggleSection.classList.toggle('active');
+
+		}
+	};
+
+	// start app
+	app.init();
+
+})();
